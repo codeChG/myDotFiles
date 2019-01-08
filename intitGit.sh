a2d504a01daa684fa55e8fed97c5770f41d727c2
Christian <ch.grimm@gmx.net>
2019-01-08 10:02:37 +0100

    initial commit

---
+++ intitGit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo init git variables
+
+git config --global user.email ch.grimm@gmx.net
+git config --global user.name Christian Grimm
---
+++ setGermanKeyboardX.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# 
+echo Hello World!
+sudo cp 20-keyboard.conf /etc/X11/xorg.conf.d/
